6adb3cbe8497265fe1df8a5d13ee61dc735ad208
Oscar Romero Silva <oscar.romero@xing.com>
2023-03-26 21:51:26 +0100

    feat(): add app icon

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,9 +1,18 @@
+import {Link} from 'react-router-dom';
 import Indicator from './Indicator';
 
 function Header() {
   return (
     <header className="flex items-center justify-between px-2 py-1 text-sky-700 shadow-md">
-      <h1 className="text-xl font-bold">Podcaster</h1>
+      <Link to="/" className="flex gap-2">
+        <img
+          src="../../../public/podcastIcon.ico"
+          alt="podcaster-icon"
+          width="30px"
+          className="object-fill rounded-md"
+        />
+        <h1 className="text-xl font-bold">Podcaster</h1>
+      </Link>
       {true && <Indicator />}
     </header>
   );
