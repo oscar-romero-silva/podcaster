5e691761770b3e056f51a4a7627dbc03044a6f51
Oscar Romero Silva <oscar.romero@xing.com>
2023-03-28 17:37:13 +0100

    feat(): export click action

--- src/app/components/PodcastsDetailsView/PodcastCardDetails.tsx
+++ src/app/components/PodcastsDetailsView/PodcastCardDetails.tsx
@@ -2,19 +2,20 @@ import Podcast from '@/domain/Podcast';
 
 type DetailsProp = {
   podcast: Podcast | null;
+  onClick: () => void;
 };
-function PodcastCardDetails({podcast}: DetailsProp) {
+function PodcastCardDetails({podcast, onClick}: DetailsProp) {
   return (
     podcast && (
-      <div className="overflow-hidden bg-white rounded-lg shadow-custom flex flex-col divide-y px-4 pb-4">
-        <img
-          className="my-6 mx-8 h-48 w-48 rounded-md self-center"
-          src={podcast?.img || ''}
-          alt="PodcastDetails"
-        />
+      <div className="bg-white rounded-lg shadow-custom flex flex-col divide-y px-4 pb-4">
+        <button className="w-auto self-center my-6" type="button" onClick={onClick}>
+          <img className="h-48 w-48 rounded-md" src={podcast?.img || ''} alt="PodcastDetails" />
+        </button>
         <div className="py-4">
-          <span className="text-md font-bold">{podcast.name}</span>
-          <p className="text-sm italic">by {podcast.artist}</p>
+          <button className="flex flex-col text-start" type="button" onClick={onClick}>
+            <span className="text-md font-bold">{podcast.name}</span>
+            <p className="text-sm italic">by {podcast.artist}</p>
+          </button>
         </div>
         <div className="pt-4">
           <span className="text-sm font-bold">Description:</span>
