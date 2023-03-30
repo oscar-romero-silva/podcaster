8aae2472fe771e6d42348a7a8ad538c600fc167d
Oscar Romero Silva <oscar.romero@xing.com>
2023-03-30 17:54:29 +0100

    feat(): add mocked data

---
+++ src/app/mocks/MockedData.ts
@@ -0,0 +1,37 @@
+export const MOCKED_PODCASTS = [
+  {
+    artist: 'The Joe 2',
+    id: 1,
+    img: 'https://image1.com',
+    name: 'The Joe Podcast',
+    summary: 'Summary',
+    title: 'First Podcast',
+  },
+  {
+    artist: 'The Joe 2',
+    id: 2,
+    img: 'https://image2.com',
+    name: 'The Joe Podcast',
+    summary: 'Summary',
+    title: 'First Podcast',
+  },
+];
+
+export const MOCKED_EPISODES = [
+  {
+    description: 'description 2',
+    duration: 7292,
+    episodeTrackUrl: 'https:/track2.com',
+    id: 1096830182,
+    name: 'Drink Champs2',
+    releaseDate: '2023-03-17T07:01:00.000Z',
+  },
+  {
+    description: 'description',
+    duration: 7292,
+    episodeTrackUrl: 'https:/track.com',
+    id: 1096830182,
+    name: 'Drink Champs',
+    releaseDate: '2023-03-17T07:01:00.000Z',
+  },
+];
