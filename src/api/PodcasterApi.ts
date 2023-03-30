a6f6b4e3851d87c91176af3bea4d5f2c46e6e2ff
Oscar Romero Silva <oscar.romero@xing.com>
2023-03-30 17:52:06 +0100

    refactor(): remove unnecessary code

--- src/api/PodcasterApi.ts
+++ src/api/PodcasterApi.ts
@@ -5,18 +5,16 @@ import {PodcastResponse, EpisodeResponse} from './types';
 
 export default class PodcasterApi extends ApiConfig implements IPodcasterApi {
   async getAllPodcasts(): Promise<PodcastResponse[]> {
-    const request = this.axios
+    return this.axios
       .get(allOrigins(`${this.path}/us/rss/toppodcasts/limit=100/genre=1310/json`))
       .then(response => response.data)
       .then(data => JSON.parse(data.contents).feed.entry);
-    return request;
   }
 
   async getPodcast(id: number): Promise<EpisodeResponse[]> {
-    const request = this.axios
+    return this.axios
       .get(allOrigins(`${this.path}/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`))
       .then(response => response.data)
       .then(data => JSON.parse(data.contents).results);
-    return request;
   }
 }
--- src/api/types.ts
+++ src/api/types.ts
@@ -19,43 +19,17 @@ export type ImageType = LabelType & {
   attributes: Pick<AttributesType, 'height'>;
 };
 
-type PriceType = LabelType & {
-  attributes: Pick<AttributesType, 'amount' | 'currency'>;
-};
-
-type ContentType = {
-  attributes: Pick<AttributesType, 'term' | 'label'>;
-};
-
-type LinkType = {
-  attributes: Pick<AttributesType, 'rel' | 'type' | 'href'>;
-};
-
 type IdType = LabelType & {
   attributes: Pick<AttributesType, 'im:id'>;
-};
-
-type CategoryType = {
-  attributes: Pick<AttributesType, 'term' | 'label'>;
-};
-
-type ReleaseDateType = LabelType & {
-  attributes: Pick<AttributesType, 'label'>;
 };
 
 export type PodcastResponse = {
   'im:name': LabelType;
   'im:image': ImageType[];
   summary: LabelType;
-  'im:price': PriceType;
-  'im:contentType': ContentType;
-  rights: LabelType;
   title: LabelType;
-  link: LinkType;
   id: IdType;
   'im:artist': LabelType;
-  category: CategoryType;
-  'im:releaseDate': ReleaseDateType;
 };
 
 export type EpisodeResponse = {
@@ -64,6 +38,5 @@ export type EpisodeResponse = {
   trackName: string;
   releaseDate: string;
   trackTimeMillis: number;
-  trackViewUrl: string;
   episodeUrl: string;
 };
