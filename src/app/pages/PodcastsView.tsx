c6f8cb0991ca3cd7dedfc373d2af217eabb6b256
Oscar Romero Silva <oscar.romero@xing.com>
2023-04-04 10:39:03 +0100

    feat(): add loading to podcasts view

--- src/app/pages/PodcastsView.tsx
+++ src/app/pages/PodcastsView.tsx
@@ -1,31 +1,39 @@
 import useFilterPodcast from '@/infrastructure/hooks/useFilterPodcasts';
 import PodcastCard from '../components/PodcastsView/PodcastCard';
 import PodcastsNotFound from '../components/PodcastsView/PodcastsNotFound';
+import Loading from '../components/Layout/Loading';
+import useLoading from '@/infrastructure/hooks/useLoading';
 
 function PodcastsView() {
   const {filteredPodcasts, filterPodcasts} = useFilterPodcast();
-
+  const {loading} = useLoading();
   return (
-    <div>
-      <div className="flex justify-center md:justify-end items-center gap-3 w-full min-w-[300px] mb-5">
-        <span className="bg-sky-700 text-white text-lg font-bold rounded-lg h-min px-2">
-          {filteredPodcasts.length}
-        </span>
-        <input
-          type="text"
-          placeholder="Filter podcasts..."
-          className="block shadow-custom rounded-lg px-3 py-2 focus:border-sky-700 focus:outline-none focus:ring focus:ring-sky-700 focus:ring-opacity-40"
-          onChange={e => filterPodcasts(e.target.value)}
-        />
-      </div>
-      {filteredPodcasts.length ? (
-        <div className="flex flex-wrap justify-center md:justify-between">
-          {filteredPodcasts.map(podcast => {
-            return <PodcastCard key={podcast.id} podcast={podcast} />;
-          })}
-        </div>
+    <div className="flex justify-center">
+      {loading ? (
+        <Loading />
       ) : (
-        <PodcastsNotFound />
+        <div>
+          <div className="flex justify-center md:justify-end items-center gap-3 w-full min-w-[300px] mb-5">
+            <span className="bg-sky-700 text-white text-lg font-bold rounded-lg h-min px-2">
+              {filteredPodcasts.length}
+            </span>
+            <input
+              type="text"
+              placeholder="Filter podcasts..."
+              className="block shadow-custom rounded-lg px-3 py-2 focus:border-sky-700 focus:outline-none focus:ring focus:ring-sky-700 focus:ring-opacity-40"
+              onChange={e => filterPodcasts(e.target.value)}
+            />
+          </div>
+          {filteredPodcasts.length ? (
+            <div className="flex flex-wrap justify-center md:justify-between">
+              {filteredPodcasts.map(podcast => {
+                return <PodcastCard key={podcast.id} podcast={podcast} />;
+              })}
+            </div>
+          ) : (
+            <PodcastsNotFound />
+          )}
+        </div>
       )}
     </div>
   );
